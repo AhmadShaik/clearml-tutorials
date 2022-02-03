0942f0ffc417132efca8eb2343bedcf1891ccf00
Recode Solutions <shaik@recodesolutions.com>
2022-02-03 17:20:08 +0530

    readme file is updated

--- tutorial_1/toy_example.py
+++ tutorial_1/toy_example.py
@@ -5,6 +5,7 @@ from clearml import Task
 task = Task.init(project_name='toy_example',
                 task_name='tensorflow_training_additioanllayer')
 
+Logger = Task.get_logger
 
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -26,4 +27,3 @@ model.compile(optimizer='adam',
 
 model.fit(x_train, y_train, epochs=5)
 model.evaluate(x_test, y_test, verbose=2)
-print(mnist)
